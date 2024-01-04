06cb70509f1f6c1c1db27a0cb35c444788044823
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2024-01-04 11:14:56 -0800

    Added mongoDB and mongoose to node.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root@bookstore-mern.48rxmew.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import {PORT} from "./config.js";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to the backend!');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
