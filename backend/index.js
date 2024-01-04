a8677dafca15142c7ad3b2d96000d073fc18e04f
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2024-01-04 13:34:41 -0800

    Followed CORS Policy to allow for custom origins

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
-import {Book} from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
 // For postman (Middleware)
 app.use(express.json());
+
+// Middleware for CORS Policy
+// Allow custom origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
 
 app.get('/', (request, response) => {
     console.log(request);
