79087380e7dfc57e28bca25c3c54c9c010cdbb9e
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2024-01-04 11:42:39 -0800

    Get request for ONE book by ID

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,20 @@ app.get('/books', async(request, response) => {
     }
 });
 
+// Route for get one book from database by id
+app.get('/books/:id', async(request, response) => {
+    try{
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
