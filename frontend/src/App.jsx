edde01ca67a6e15c8166d2757aade25e251d573b
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2024-01-04 14:38:16 -0800

    Added skeleton for front page and links to other pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import CreateBook from './pages/CreateBooks'
+import CreateBooks from './pages/CreateBooks'
 import EditBook from './pages/EditBook'
 import ShowBook from './pages/ShowBook'
 import DeleteBook from './pages/DeleteBook'
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/create" element={<CreateBooks />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
