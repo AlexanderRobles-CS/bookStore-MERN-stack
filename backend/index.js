01471ebe6676d531506e01d555a61c75814cac4b
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2024-01-04 11:48:13 -0800

    Made request for updating a book by ID

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,32 @@ app.get('/books/:id', async(request, response) => {
     }
 });
 
+// Route for update a book from database by id
+app.put('/books/:id', async(request, response) => {
+    try{
+        if(!request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({message: 'Send all required fields" title, author, publishYear'
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result) {
+            return response.status(404).send({message: 'Book not found'});
+        }
+
+        return response.status(200).send({message: 'Book updated'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
